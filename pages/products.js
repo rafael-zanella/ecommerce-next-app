d542c7363e569727b18da66af669ee9c29889e53
Rafael Zanella <rafaels.zanella@gmail.com>
2021-02-09 19:16:31 -0300

    feat: add products page.

---
+++ pages/products.js
@@ -0,0 +1,11 @@
+const Products = () => {
+  return (
+    <>
+      <h1>Cool product 1</h1>
+      <p>This is a very cool product</p>
+      <p>$2.50</p>
+    </>
+  )
+}
+
+export default Products;
